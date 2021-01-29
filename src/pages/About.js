bd41684f9b91d9dc8e3cb2b7a179038e23172596
tiagohisun <tiagohisun@gmail.com>
2021-01-29 13:37:55 -0300

    3

--- src/pages/About.js
+++ src/pages/About.js
@@ -21,190 +21,190 @@ const About = () => {
               <h5 className="mb-3">About Us</h5>
             </Link>
             <Link to="/about#welcome">
-              <p>Bem Vindos à DENTAL04!</p>
+              <p>Welcome to DENTAL04!</p>
             </Link>
             <Link to="/about#ourVision">
-              <p>Nossa Visão</p>
+              <p>Our Vision</p>
             </Link>
             <Link to="/about#ourQuality">
-              <p>Nossa Qualidade</p>
+              <p>Our Quality</p>
             </Link>
             <Link to="/about#aboutDental">
-              <p>Sobre a DENTAL04</p>
+              <p>About DENTAL04</p>
             </Link>
             <Link to="/about#ourMission">
-              <p>Nossa Missão</p>
+              <p>Our Mission</p>
             </Link>
             <Link to="/about#privacy">
-              <p>Política de Privacidade</p>
+              <p>Privacy Policy</p>
             </Link>
             <Link to="/about#cookies">
               <p>Cookies</p>
             </Link>
             <Link to="/about#delivery">
-              <p>Política de Entrega</p>
+              <p>Delivery Policy</p>
             </Link>
             <Link to="/about#guidance">
-              <p>Restrições e Guias</p>
+              <p>Guides and Restrictions</p>
             </Link>
             <Link to="/about#warranty">
-              <p>Política de Garantia</p>
+              <p>Warranty Policy</p>
             </Link>
             <Link to="/about#security">
-              <p>Segurança</p>
+              <p>Security</p>
             </Link>
           </div>
         </div>
 
         <div className="col-sm-6 ml-2 pt-4" style={{ left: "110px" }}>
           <h2 className="pb-3">About Us</h2>
-          <h5 id="welcome">Bem Vindos à DENTAL04!</h5>
+          <h5 id="welcome">Welcome to DENTAL04!</h5>
           <p>
-            A empresa foi fundada em Setembro de 2018 por técnicos com mais de
-            10 anos de experiência no ramo de assistência técnica, com o
-            objetivo de fornecer um serviço aprimorado e diferenciado.
+            Welcome to DENTAL04!
+            The company was founded in September 2018 by technicians 
+            with over 10 years of experience in the field of technical 
+            assistance, in order to provide an improved and differentiated service.
           </p>
-          <h5 id="">Quem Somos Nós</h5>
+          <h5 id="">Who are we</h5>
           <p>
-            A DENTAL04 é uma empresa que presta serviços de assistência técnica,
-            anúncios de aparelhos de raios-x e programas odontológicos –
-            provenientes de clínicas de radiologia e consultórios odontológicos.
-            Contamos também com o suporte aos produtos do programa ONDEMAND3D de
-            diagnóstico para tomografias usado mundialmente.
+            DENTAL04 is a company that provides technical assistance services,
+            advertisements for x-ray machines and dental programs - from radiology 
+            clinics and dental offices. We also have support for the products of 
+            the ONDEMAND3D diagnostic program for CT scans used worldwide.
           </p>
-          <h5 id="ourVision">Nossa Visão</h5>
+          <h5 id="ourVision">Our Vision</h5>
           <p>
-            Hoje atendemos diversas clínicas de radiologia em todo território
-            brasileiro. Fornecemos assistência rápida e de qualidade, além de
-            ajudarmos nas vendas de seus aparelhos de raios-x usados. Mais
-            satisfação e confiança em nossos serviços.
+            Today we serve several radiology clinics throughout the territory
+            Brazilian. We provide fast, quality assistance and
+            help you sell your used x-ray machines. More
+            satisfaction and confidence in our services.
           </p>
-          <h5 id="ourQuality">Nossa Qualidade</h5>
-          <p>Fornecemos serviços íntegros e de qualidade. Experimente!</p>
-          <h5 id="aboutDental">Sobre a DENTAL04</h5>
-          A DENTAL04 é uma empresa que presta serviços de assistência técnica,
-          anúncio de aparelhos de raios-x e programas odontológicos provenientes
-          de clínicas de radiologia e consultórios odontológicos além de à
-          alguns programas de diagnósticos tais como ONDEMAND3D usados
-          mundialmente. A empresa nasceu em Setembro de 2018 por meio de
-          técnicos com mais de 10 anos de experiência no ramo de assistência
-          técnica com o objetivo de fornecer uma assistência aprimorada e
-          diferenciada. Hoje atendemos diversas clínicas de radiologia em todo
-          território brasileiro, fornecendo assistência rápida e de qualidade,
-          além de ajudarmos nas vendas de seus aparelhos de raios-x usados
-          gerando mais satisfação e confiança em nossos serviços.
+          <h5 id="ourQuality">Our Quality</h5>
+          <p>We provide integrity and quality services. Try it!</p>
+          <h5 id="aboutDental">About DENTAL04</h5>
+          DENTAL04 is a company that provides technical assistance services,
+          announcement of x-ray machines and dental programs from
+          radiology clinics and dental offices in addition to the
+          some diagnostic programs such as ONDEMAND3D used
+          worldwide. The company was born in September 2018 through
+          technicians with over 10 years of experience in the service industry
+          technical assistance with the aim of providing enhanced and
+          differentiated. Today we serve several radiology clinics throughout
+          Brazilian territory, providing fast and quality assistance,
+          in addition to helping to sell your used x-ray machines
+          generating more satisfaction and confidence in our services.
           <br />
           <br />
-          <h5 id="ourMission"> Nossa Missão </h5>
+          <h5 id="ourMission"> Our Mission </h5>
           A missão da DENTAL04 é fornecer assistência rápida e eficaz à seus
           clientes, bem como ajudar nos trâmites de itens usados de suas
           clínicas.
           <br />
           <br />
-          <h5 id="privacy"> Política de Privacidade </h5>
-          Priorizamos a privacidade de nossos visitantes à este site por isso,
-          disponibilizamos abaixo os tipos de informações que são coletadas e
-          manuseadas. Para mais informações contate-nos através do e-mail
-          tiagohisun@gmail.com. Arquivos de Log A DENTAL04 segue o procedimento
-          padrão de uso dos arquivos de log. Esses arquivos logam os visitantes
-          enquanto eles visitam o sites. As empresas fazem isso e é parte do
-          serviço analítico dos servidores. As informações coletadas incluem
-          endereços de IP, tipo de página ISP, data, páginas de referência/saída
-          e possívelmente o número de cliques. Essas informações de log não
-          estão ligadas à nenhuma informação pessoal indentificável. Seu
-          propósito é somente analizar as tendências, administrando o site,
-          rastreando os movimentos dos usuários.
+          <h5 id="privacy"> Privacy Policy </h5>
+          We prioritize the privacy of our visitors to this site so we 
+          provide below the types of information that are collected 
+          and handled. For more information, contact us at tiagohisun@gmail.com. 
+          Log Files DENTAL04 follows the standard procedure for using log files. 
+          These files log visitors while they visit the site. 
+          Companies do this and it is part of the analytical service of servers. 
+          The information collected includes IP addresses, ISP page type, date, 
+          referral / exit pages and possibly the number of clicks. 
+          This log information is not linked to any personally identifiable 
+          information. Its purpose is only to analyze trends, managing the site, 
+          tracking users' movements.
           <br />
           <br />
-          <h5 id="cookies"> Cookies e Web Beacons </h5>
-          Como qualquer outro website, a DENTAL04 utiliza 'cookies'. Eles são
-          usados para armazenar informações que incluem, as preferências dos
-          visitantes e as páginas que eles acessam. São usados para otimizar a
-          experiência do usuário baseadas nessas informações. Política de
-          Privacidade de Terceiros A política de privacidade da DENTAL04 não se
-          aplica à nenhum outro anunciante ou sites de terceiros. Sendo assim,
-          aconselhamos à consultar suas respectivas políticas de privacidade
-          Você poderá desabilitar a função de cookies nas opções de sua página.
-          Não colhemos informações alguma de indivíduos abaixo de 18 anos. Caso
-          ache algum equívoco em algum cadastro, por favor, nos informe para
-          removermos o mais rápido tais informações de nossos registros. Esta
-          Política de Privacidade se aplica somente à nossas atividades online e
-          é válida aos visitantes de nosso site concernente as informações que
-          os mesmos compartilham ou coletam da DENTAL04. Esta política não se
-          aplica à nenhuma informação coletada offline ou por outros meios à
-          este site. Acordo Usando este site, você declara estar ciente de nossa
-          Política de Privacidade e concorda com seus Termos e Condições.
+          <h5 id="cookies"> Cookies and Web Beacons </h5>
+          
+          Like any other website, DENTAL04 uses 'cookies'. They are
+          used to store information that includes, user preferences
+          visitors and the pages they visit. They are used to optimize
+          user experience based on that information. Policy
+          Third Party Privacy DENTAL04's privacy policy does not apply
+          applies to no other advertisers or third party sites. Therefore,
+          we advise you to consult their respective privacy policies
+          You can disable the cookies function in the options on your page.
+          We do not collect any information from individuals under the age of 18. Case
+          find any mistake in any registration, please inform us to
+          remove such information from our records as soon as possible. It is
+          Privacy Policy applies only to our online activities and
+          is valid for visitors to our website regarding the information they
+          they share or collect DENTAL04. This policy is not
+          applies to any information collected offline or by other means to
+          this site. Agreement Using this website, you declare to be aware of our
+          Privacy Policy and agree to its Terms and Conditions.
           <br />
           <br />
-          <h5 id="delivery"> Política de Entrega, Prazos e Valores </h5>
-          Quanto ao Transporte: Nas negociações de vendas dos aparelhos de
-          raios-x, a DENTAL04 não fornece transporte aos mesmos em nenhum caso.
-          Por isso, os preços e prazos de transporte estão por conta das
-          transportadoras escolhidas por uma das partes (comprador ou vendedor).
-          Quanto aos Suportes Remotos e Presenciais: Os valores e prazos médios
-          de assistência remota e presencial poderão ser cotados mediante a
-          solicitação do cliente com a empresa. Transportadoras: A empresa
-          transportadora dos aparelhos no caso das vendas deles, estão à cargo
-          da escolha das partes do negócio tratado, isto é; comprador ou
-          vendedor do produto negociado. Opções de Pagamento: Quanto Ao
-          Transporte: A DENTAL04 não recebe pagamento algum relativo à
-          transporte das máquinas negociadas. O mesmo deverá ser tratado
-          diretamente com a transportadora. Quanto À Assistência Presencial Ou
-          Remota: A DENTAL04 não recebe pagamento antecipado relativo ao seus
-          suportes presenciais e remotos. Sempre é solicitado ao comprador do
-          serviço (cliente) à realizar o pagamento após o serviço ser concluído
-          com sucesso ou o problema diagnosticado.
+          <h5 id="delivery"> Delivery, Terms and Price Policies </h5>
+          Regarding Transport: In the sales negotiations for handsets
+          x-rays, DENTAL04 does not provide transport in any case.
+          For this reason, prices and transportation terms are due to the
+          carriers chosen by one of the parties (buyer or seller).
+          Regarding Remote and Face-to-Face Support: The average values ​​and terms
+          remote and in-person assistance may be quoted by
+          customer request with the company. Carriers: The company
+          carrier of the devices in the case of their sales, are in charge
+          the choice of parts of the deal dealt with, that is; buyer or
+          seller of the traded product. Payment Options: As for
+          Transport: DENTAL04 does not receive any payment related to
+          transportation of the negotiated machines. It should be treated
+          directly with the carrier. Regarding On-site Assistance Or
+          Remote: DENTAL04 does not receive advance payment for its
+          face-to-face and remote support. The buyer of the
+          service (customer) to make the payment after the service is completed
+          successfully or the problem diagnosed.
           <br />
           <br />
-          <h5 id="guidance"> Restrições e Guias </h5>
-          Quanto À Assistência Presencial Ou Remota: A DENTAL04 atende a todas
-          regiões incluindo exterior nos idiomas Português e Inglês. Quanto Ao
-          Meio de Transporte ou Acesso: Política de Troca e Devoluções No Caso
-          de Transporte: Nas vendas realizadas ou intermediadas pela DENTAL04,
-          fica a cargo do dono do aparelho (vendedor) decidir mediante a
-          contrato o prazo ou política de devolução do aparelho caso o mesmo
-          apresente defeitos. Tal política é informada ao comprador antes do ato
-          (fechamento) da venda. No Caso de Acesso Remoto: O Meio principal
-          utilizado para acesso é o programa de acesso teamviewer, com o título
-          da empresa no acesso. Por motivos necessários, poderá ser usado outros
-          programas de acesso, SEMPRE com o consentimento prévio do cliente.
+          <h5 id="guidance"> Guidance and Restrictions </h5>
+          Regarding On-site or Remote Assistance: DENTAL04 meets all
+          regions including abroad in Portuguese and English. About the
+          Means of Transport or Access: Exchange Policy and Returns In Case
+          Transport: In sales made or intermediated by DENTAL04,
+          it is the responsibility of the device owner (seller) to decide upon
+          contract the term or policy for returning the device if it
+          be defective. Such policy is informed to the buyer before the act
+          (closing) of the sale. In the case of Remote Access: The main means
+          used for access is the teamviewer access program, with the title
+          company's access. For necessary reasons, other
+          access programs, ALWAYS with the client's prior consent.
           <br />
           <br />
-          <h5 id="warranty"> Política de Garantia </h5>
-          A DENTAL04 não fornece garantia nas intermediações feitas pela mesma
-          nas vendas dos aparelhos. Porém é lei federal conforme Código de
-          Defesa do Consumidor – Lei 8.078/90 o mínimo de 3 meses em qualquer
-          venda realizada contra defeitos do aparelho, sendo tal defeito oriundo
-          da máquina, não provocado por falhas de terceiros.
+          <h5 id="warranty"> Warranty Policy </h5>
+         DENTAL04 does not provide a guarantee for intermediaries made by the same
+          sales of handsets. However, it is federal law according to the
+          Consumer Protection - Law 8,078 / 90 the minimum of 3 months in any
+          sale made against device defects, such defect originating from
+          of the machine, not caused by third party failures.
           <br />
           <br />
-          <h5 id="security"> Política de Segurança </h5>
-          Embora a DENTAL04 aplique certos filtros de segurança no tratamento de
-          vendas das máquinas em que ela está envolvida, ainda sim é recomendado
-          observar as precauções padrão e adcionais na venda das máquinas
-          realizadas cliente-à-cliente para mais segurança das partes do negócio
-          tratado, tais como: 1. Conheça ao máximo o comprador, observe se o
-          mesmo possui local de trabalho físico (clínica, consultório,etc),
-          característico de potencial comprador de aparelhosde raios-x. 2. Não
-          forneça nenhum dado pessoal de documentos sem averiguar quem é o
-          possível comprador/vendedor. 3. Jamais compre ou venda se fazer um
-          contrato em que ambas as partes registrem suas assinaturas em
-          cartório. 4. Jamais baseie a compra ou venda somente por textos ou
-          mensagens de aplicativos, se ambas as partes não se conhecem. Mesmo
-          assim, perceba quaisquer tramas sutis. 5. Jamais se encontre
-          pessoalmente sem ter conhecimento do comprador ou vendedor
-          previamente. 6. Fique atento à possíveis mensagens enganosas de
-          solicitação de pagamentos, ou dados pessoais principalmente
-          antecipados. 7. Grave todas as mensagens e ligações ou vídeo chamadas
-          das conversas nas negociações. 8. Obtenha o número de série do
-          aparelho ou produto e rastreie-o.
+          <h5 id="security"> Security Policy </h5>
+          Although DENTAL04 applies certain safety filters in the treatment of
+          sales of the machines in which it is involved, it is still recommended
+          observe standard and additional precautions when selling machines
+          carried out customer-to-customer for more security of parts of the business
+          treated, such as: 1. Get to know the buyer as much as possible,
+          even has a physical workplace (clinic, office, etc.),
+          characteristic of a potential buyer of x-ray devices. 2. No
+          provide no personal data from documents without finding out who the
+          possible buyer / seller. 3. Never buy or sell if you make one
+          contract in which both parties register their signatures in
+          registry. 4. Never base your purchase or sale solely on texts or
+          application messages, if both parties do not know each other. Same
+          so, notice any subtle plots. 5. Never meet
+          personally without being aware of the buyer or seller
+          previously. 6. Watch out for possible misleading messages from
+          request for payments, or personal data mainly
+          anticipated. 7. Record all messages and calls or video calls
+          of the talks in the negotiations. 8. Obtain the serial number of the
+          device or product and track it.
         </div>
         <div className="col-sm-3 pt-4" style={{ width: "150px" }}>
           <div className="card ml-2 mt-5" style={{ left: "110px", width: "250px" }}>
             <img className="card-img-top" src={repair} alt="Card image cap" />
             <div className="card-body">
               <p className="card-text">
-                Some quick example text to build on the card title and make up
+               Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </p>
             </div>
