44bdffa93b39f883472113866bb92fb0affd286b
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2021-02-02 20:50:13 +0500

    Add product contact form with email

--- src/components/cards/SingleProduct.js
+++ src/components/cards/SingleProduct.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Card, Tabs } from "antd";
+import React,{useState} from "react";
+import { Card, Tabs,Modal,Button ,notification,Input} from "antd";
 import { Link } from "react-router-dom";
 import { HeartOutlined, ContactsOutlined } from "@ant-design/icons";
 import { Carousel } from "react-responsive-carousel";
@@ -9,13 +9,65 @@ import ProductListItems from "./ProductListItems";
 import StarRating from "react-star-ratings";
 import RatingModal from "../modal/RatingModal";
 import { showAverage } from "../../functions/rating";
-
+import axios from 'axios'
 const { TabPane } = Tabs;
 
 // this is childrend component of Product page
 const SingleProduct = ({ product, onStarClick, star }) => {
   const { model, images, description, _id } = product;
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  console.log("Product=>",product)
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const openNotification = (message,description) => {
+    notification.open({
+      message: message,
+      description:description,
+      onClick: () => {
+        console.log('Notification Clicked!');
+      },
+    });
+  };
 
+  const handleOk = () => {
+    if (name === "" || email === "" || message === "") {
+      alert("please fill the form");
+    } else {
+      var data = 'This is your data from here';
+
+      axios
+        .post(`${process.env.REACT_APP_API}/email`, {
+          name: name,
+          from: email,
+          text: `ID : ${product._id} 
+                 Title : ${product.title}
+                 Price : ${product.price}
+                 Brand : ${product.brand}
+                 Location : ${product.location}
+                 Description : ${product.description}
+                 Message : ${message}
+          `,
+        })
+        .then((r) => {
+          openNotification("Message Send Successfully", "We will contact you as soon as possible")
+          setIsModalVisible(false);
+          setEmail('')
+          setMessage('')
+          setName('')
+        }
+        )
+        .catch((e) => openNotification("Something error here","Check your internet connecting..."));
+    }
+    
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
     <>
       <div className="col-md-7">
@@ -46,7 +98,7 @@ const SingleProduct = ({ product, onStarClick, star }) => {
           <div className="text-center pt-1 pb-3">No rating yet</div>
         )}
 
-        <Card
+        <Card onClick={showModal}
           actions={[
             <>
               <ContactsOutlined className="text-success" /> <br />
@@ -67,6 +119,26 @@ const SingleProduct = ({ product, onStarClick, star }) => {
         >
           <ProductListItems product={product} />
         </Card>
+        <Modal title="Contact Us" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}
+          footer={[
+            <Button key="back" onClick={handleCancel}>Back</Button>,
+            <Button key="enter" onClick={handleOk}>Send Message</Button>,
+            
+        ]}
+        >
+          <p>ID : { product._id }</p>
+          <p>Title : { product.title }</p>
+          <p>Brand : { product.brand }</p>
+          <p>Location : {product.location}</p>
+          <p>Price : { product.price }</p>
+          <p>Description : {product.description}</p>
+          <p>Email </p>
+          <Input value={email} type="email" onChange={(e)=>setEmail(e.target.value)} />
+          <p>Name </p>
+          <Input value={name} type="text" onChange={(e)=>setName(e.target.value)} />
+          <p>Message</p>
+          <Input.TextArea rows="4" value={message} onChange={(e)=>setMessage(e.target.value)} />
+        </Modal>
       </div>
     </>
   );
