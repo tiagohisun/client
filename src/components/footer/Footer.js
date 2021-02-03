a3c7c5705974b34ee5394603a1a2ee5837a96133
tiagohisun <tiagohisun@gmail.com>
2021-02-02 23:18:32 -0300

    1

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,71 @@
 import React, { useState } from "react";
+import { Form, Input, Button, notification } from "antd";
 import { Link } from "react-router-dom";
+import axios from "axios";
 //import { AiFillPhone } from "react-icons/ai";
 import { MdEmail, MdLocationOn } from "react-icons/md";
 import logo from "../../static/images/logo/logonb.png"
+import '../../static/css/footer.css';
 //import emailjs from "emailjs-com";
 const Footer = () => {
+  const [ isModalVisible, setIsModalVisible ] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phonenumber, setPhonenumber] = useState(null);
+  const [message, setMessage] = useState("Newsletter");
+  
+  const showModal = () => {
+		setIsModalVisible(true);
+	};
+
+  const openNotification = (message,description) => {
+    notification.open({
+      message: message,
+      description:description,
+      onClick: () => {
+        console.log('Notification Clicked!');
+      },
+    });
+  };
+
+
+  const mailsender = () => {
+    if (name === "" || email === "" || message === "") {
+      alert("please fill the form");
+    } else {
+      var data = 'This is your data from here';
+
+      axios
+        .post(`${process.env.REACT_APP_API}/email`, {
+          name: name,
+          from: email,
+          text: `
+          Name: ${name}
+          Email: ${email}
+          Phone: ${phonenumber}
+          Interested in : ${message}
+          `
+        })
+        .then((r) => {
+          openNotification("Email Send Successfully", "We will contact you as soon as possible")
+          setIsModalVisible(false);
+          setEmail('')
+          setMessage('')
+          setName('')
+          setPhonenumber('')
+        }
+        )
+        .catch((e) => openNotification("Something error here","Check your internet connecting..."));
+    }
+  };
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 12,
+    },
+  };
   //const [message, setMessage] = useState("");
   //function sendEmail(e) {
   //  e.preventDefault();
@@ -124,48 +185,62 @@ const Footer = () => {
           </div>
           <div className="col-sm-12 col-md-3 mt-5">
             
-            {/* <form >
-              <label>Weekly Newslatter</label>
-              <br />
-              <input
-                type="text"
-                name="to_name"
-                style={{
-                  width: "80%",
-                  backgroundColor: "#afb4bf",
-                  padding: "5px",
-                  border: "0px",
-                  margin: "10px 0px",
-                }}
-                placeholder="Your Name"
-              />
-              <input
-                type="text"
-                name="from_name"
-                style={{
-                  width: "80%",
-                  backgroundColor: "#afb4bf",
-                  padding: "5px",
-                  border: "0px",
-                  margin: "10px 0px",
-                }}
-                placeholder="Your Email"
-              />
-              <br />
-              <input
-                type="submit"
-                style={{
-                  width: "80%",
-                  backgroundColor: "blue",
-                  fontWeight: "bold",
-                  color: "white",
-                  padding: "5px",
-                  border: "0px",
-                  margin: "10px 0px",
-                }}
-                value="Subscribe"
-              />
-            </form> */}
+           <div style={{ marginTop: "30px" }}>
+
+           
+      <Form style={{width:"200px"}}>
+        
+          <input style={{height:"25px", marginBottom:"12px", border:"none", color:"black",  width:"200px", textAlign: "center", backgroundColor:"lightGray"}}
+            type="text"
+            placeholder="Your Name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        
+        
+          <input style={{height:"25px", marginBottom:"12px", border:"none", color:"black",  width:"200px", textAlign: "center", backgroundColor:"lightGray"}}
+            type="email"
+            placeholder="Your Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        
+  
+          <input style={{height:"25px", marginBottom:"12px", border:"none", color:"black", width:"200px", textAlign: "center", backgroundColor:"lightGray"}}
+            type="text"
+            placeholder="Your Phone Number"
+            value={phonenumber}
+            onChange={(e) => setPhonenumber(e.target.value)}
+          />
+        
+
+        <Form.Item label="Message" hidden={true}>
+          <Input.TextArea 
+            type="hidden"
+            rows={4}
+            placeholder="Enter Your Message......!"
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </Form.Item>
+        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 10 }}>
+          <div style={{ textAlign: "center"}}>
+            <Button
+              style={{right:"83px", width:"200px", textAlign: "center" }}
+              type="primary"
+              htmlType="submit"
+              onClick={() => mailsender()}
+              block
+            >
+              Submit
+            </Button>
+          </div>
+        </Form.Item>
+      </Form>
+    </div>
           </div>
         </div>
       </div>
