08f19d9219b3e45408fddaf2f254b28e6e2d0250
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2021-02-12 21:39:07 +0500

    service page is mobile friendly

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,117 +1,114 @@
-import React, { useState } from "react";
-import { Form, Input, Button, notification } from "antd";
-import axios from "axios";
-import {useTranslation} from 'react-i18next'
+import React, { useState } from 'react';
+import { Form, Input, Button, notification } from 'antd';
+import axios from 'axios';
+import { useTranslation } from 'react-i18next';
 const Contact = () => {
-  const {t, i18n} = useTranslation();
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phonenumber, setPhonenumber] = useState(null);
-  const [message, setMessage] = useState("");
+	const { t, i18n } = useTranslation();
+	const [ name, setName ] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ phonenumber, setPhonenumber ] = useState(null);
+	const [ message, setMessage ] = useState('');
 
-  const openNotification = (message,description) => {
-    notification.open({
-      message: message,
-      description:description,
-      onClick: () => {
-        console.log('Notification Clicked!');
-      },
-    });
-  };
+	const openNotification = (message, description) => {
+		notification.open({
+			message: message,
+			description: description,
+			onClick: () => {
+				console.log('Notification Clicked!');
+			}
+		});
+	};
 
+	const mailsender = () => {
+		if (name === '' || email === '' || message === '') {
+			alert('please fill the form');
+		} else {
+			var data = 'This is your data from here';
 
-  const mailsender = () => {
-    if (name === "" || email === "" || message === "") {
-      alert("please fill the form");
-    } else {
-      var data = 'This is your data from here';
-
-      axios
-        .post(`${process.env.REACT_APP_API}/email`, {
-          name: name,
-          from: email,
-          text: 
-         `Name: ${name}
+			axios
+				.post(`${process.env.REACT_APP_API}/email`, {
+					name: name,
+					from: email,
+					text: `Name: ${name}
           Email: ${email} 
           Phone: ${phonenumber}
           Message: ${message}
           `
-        })
-        .then((r) => {
-          openNotification("Email Send Successfully", "We will contact you as soon as possible")
-          setEmail('')
-          setMessage('')
-          setName('')
-          setPhonenumber('')
-        }
-        )
-        .catch((e) => openNotification("Something error here","Check your internet connecting..."));
-    }
-  };
-  const layout = {
-    labelCol: {
-      span: 8,
-    },
-    wrapperCol: {
-      span: 12,
-    },
-  };
+				})
+				.then((r) => {
+					openNotification('Email Send Successfully', 'We will contact you as soon as possible');
+					setEmail('');
+					setMessage('');
+					setName('');
+					setPhonenumber('');
+				})
+				.catch((e) => openNotification('Something error here', 'Check your internet connecting...'));
+		}
+	};
+	const layout = {
+		labelCol: {
+			span: 8
+		},
+		wrapperCol: {
+			span: 12
+		}
+	};
 
-  return (
-    <div style={{ marginTop: "30px" }}>
-      <Form {...layout} name="nest-messages" >
-        <Form.Item name="name" label={t("N")} rules={[ { type: 'string', required: true } ]} >
-          <Input
-            type="text"
-            placeholder={t("YN")}
-            required
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </Form.Item>
-        <Form.Item name="email" label="Email" rules={[ { type: 'email', required: true } ]}>
-          <Input
-            type="email"
-            placeholder={t("YE")}
-            required
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Item>
-        <Form.Item name="phonenumber" label={t("P")} rules={[ { type: 'string' } ]}>
-          <Input
-            type="text"
-            placeholder={t("YP")}
-            value={phonenumber}
-            onChange={(e) => setPhonenumber(e.target.value)}
-          />
-        </Form.Item>
+	return (
+		<div style={{ marginTop: '30px' }}>
+			<Form {...layout} name="nest-messages">
+				<Form.Item name="name" label={t('N')} rules={[ { type: 'string', required: true } ]}>
+					<Input
+						type="text"
+						placeholder={t('YN')}
+						required
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</Form.Item>
+				<Form.Item name="email" label="Email" rules={[ { type: 'email', required: true } ]}>
+					<Input
+						type="email"
+						placeholder={t('YE')}
+						required
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</Form.Item>
+				<Form.Item name="phonenumber" label={t('P')} rules={[ { type: 'string' } ]}>
+					<Input
+						type="text"
+						placeholder={t('YP')}
+						value={phonenumber}
+						onChange={(e) => setPhonenumber(e.target.value)}
+					/>
+				</Form.Item>
 
-        <Form.Item name="message" label={t("M")} rules={[{required:true}]}>
-          <Input.TextArea
-            rows={4}
-            placeholder={t("YMA")}
-            required
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 10 }}>
-          <div style={{ textAlign: "center", marginRight: "257px" }}>
-            <Button
-              style={{ textAlign: "center" }}
-              type="primary"
-              htmlType="submit"
-              onClick={() => mailsender()}
-              block
-            >
-              {t("SB")}
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+				<Form.Item name="message" label={t('M')} rules={[ { required: true } ]}>
+					<Input.TextArea
+						rows={4}
+						placeholder={t('YMA')}
+						required
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+					/>
+				</Form.Item>
+				<Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 10 }}>
+					<div style={{ textAlign: 'center', marginRight: '257px' }}>
+						<Button
+							style={{ textAlign: 'center' }}
+							type="primary"
+							htmlType="submit"
+							onClick={() => mailsender()}
+							block
+						>
+							{t('SB')}
+						</Button>
+					</div>
+				</Form.Item>
+			</Form>
+		</div>
+	);
 };
 
 export default Contact;
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,146 +1,176 @@
-import React, { useEffect, useState } from "react";
-import { GrCloudlinux } from 'react-icons/gr'
-import {AiOutlineBulb} from 'react-icons/ai'
-import {MdLaptopChromebook} from 'react-icons/md'
-import {RiBuilding2Line} from 'react-icons/ri'
-import { useSelector } from "react-redux";
-import { getRelated } from "../functions/product";
-import ProductCard from "../components/cards/ProductCard";
-import sales from "../static/images/solutions/sales.png"; // with import
-import leadership from "../static/images/solutions/leadership.png"
-import support from "../static/images/solutions/support.png"
-import news from "../static/images/solutions/news.png"
-import classifieds from "../static/images/solutions/classifieds.jpg" 
-import diagnostic from "../static/images/solutions/diagnostic.jpg"
-import pan from "../static/images/solutions/pan.jpg"
-import repair from"../static/images/solutions/repair.jpg"
-import {useTranslation} from 'react-i18next'
+import React, { useEffect, useState } from 'react';
+import { GrCloudlinux } from 'react-icons/gr';
+import { AiOutlineBulb } from 'react-icons/ai';
+import { MdLaptopChromebook } from 'react-icons/md';
+import { RiBuilding2Line } from 'react-icons/ri';
+import { useSelector } from 'react-redux';
+import { getRelated } from '../functions/product';
+import ProductCard from '../components/cards/ProductCard';
+import sales from '../static/images/solutions/sales.png'; // with import
+import leadership from '../static/images/solutions/leadership.png';
+import support from '../static/images/solutions/support.png';
+import news from '../static/images/solutions/news.png';
+import classifieds from '../static/images/solutions/classifieds.jpg';
+import diagnostic from '../static/images/solutions/diagnostic.jpg';
+import pan from '../static/images/solutions/pan.jpg';
+import repair from '../static/images/solutions/repair.jpg';
+import { useTranslation } from 'react-i18next';
 //const logo = require('./logo.jpeg); // with require
-
+import styles from './Services.module.css';
 const Service = () => {
-    const {t, i18n} = useTranslation();
-    return (
-        <>
-        <div className="container-fluid">
-        <div className="row mt-5 mb-5">
-            <div className="col-12">
-                <div
-                    style={{
-                        position: "relative",
-                        color: 'white'
-                    }}>
-                  
-                   
-                </div>
-            </div>
-            <div className="col-12 mt-5 text-center">
-                    <div style={{padding:'0px 25%'}}>
-                        <h1 style={{fontSize:'40px',fontWeight:'bold',marginBottom:'20px'}}>{t("SERVIÇO COMPLETO")}</h1>
-                        <p>{t("WP")}
-                        {t("WA")}</p>
-                    </div>
-            </div>
-            <div className="col-12 mt-5">
-                <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <div className="ml-4">
-                        <img className="ml-5" width="40%" src={support} alt="Card image" />
-                        </div>
-                        <div className="text-center mr-3">
-                            <h3 style={{marginTop: "42.5px"}}>{t("TS")}</h3>
-                            <p> {t("WeP")}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col">
-                    <div className="ml-4">
-                    <img className="ml-5" width="40%" src={sales} alt="Card image" />
-                        </div>
-                        <div className="text-center mr-3">
-                            <h3 style={{marginTop: "62.5px"}}>{t("SA")}</h3>
-                            <p>{t("AS")}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col">
-                    <div className="ml-4">
-                    <img className="ml-5" width="40%" src={leadership} alt="Card image" />
-                        </div>
-                        <div className="mr-3 text-center">
-                            <h3 style={{marginTop: "40px"}}> {t("GUI")}</h3>
-                            <p>{t("WePA")}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="col">
-                    <div className="ml-4">
-                    <img className="ml-5" width="40%" src={news} alt="Card image" />
-                        </div>
-                        <div className="text-center mr-3">
-                            <h3 style={{marginTop: "40px"}}> {t("NE")}</h3>
-                            <p>{t("TL")}
-                            </p>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            <div className="col-12 mt-5">
-                <div className="container bg-light">
-                     <div className="row mt-3">
-                        <div className="col m-3">
-                            <h3 className="text-center">{t("OPA")}</h3> <br/>
-                            <p className="text-center">{t("TC")}
-                            </p>
-                        </div>
-                    </div>
-                    <h3 className="text-center h2">{t("DI")}</h3> <br/>
-                    <div className="row m-1">
-                        <div className="col-md-3">
-                            <img style={{minWidth:"250px", maxWidth: "250px", minHeight:"190px",  maxHeight:"190px"}} src={classifieds} alt="classifieds" />
-                            <p style={{marginTop:"10px"}}>{t("DO")}</p>
-                        </div>
-                        <div className="col-md-3">
-                            <img style={{minWidth:"250px", maxWidth: "250px", minHeight:"190px",  maxHeight:"190px"}} src={diagnostic} alt="diagnnostic" />
-                            <p style={{marginTop:"10px"}}>{t("CB")}</p>
-                        </div>
-                        <div className="col-md-3">
-                            <img style={{minWidth:"250px", maxWidth: "250px", minHeight:"190px",  maxHeight:"190px"}} src={pan} alt="pan" />
-                            <p style={{marginTop:"10px"}}>{t("D2")} - {t("QA")}</p>
-                        </div>
-                        <div className="col-md-3 ">
-                            <img style={{minWidth:"250px", maxWidth: "250px", minHeight:"190px",  maxHeight:"190px"}} src={repair} alt="repair" />
-                            <p style={{marginTop:"10px"}}>{t("DU")}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="col-md-12 bg-dark">
-                <div className="container text-white">
-                    <div className="row">
-                        <div className="col mt-5 mb-5 text-center">
-                            <div className="mt-5">
-                                <h1 className="text-white">{t("WL")}</h1>
-                                <a href="/contact">
-                                <button
-                                style={{padding:'10px 40px',
-                                backgroundColor:'transparent',
-                                border:"1px solid white",
-                                color:'white',
-                                borderRadius:'3px',
-                                marginTop:'10px'
-                            }}
-                                >{t("CTL")}</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </>
-    )
-}
+	const { t, i18n } = useTranslation();
+	return (
+		<React.Fragment>
+			<div className="container-fluid">
+				<div className="row mt-5 mb-5">
+					<div className="col-12">
+						<div
+							style={{
+								position: 'relative',
+								color: 'white'
+							}}
+						/>
+					</div>
+					<div className="col-12 mt-5 text-center">
+						<div className={styles.headingParagraph}>
+							<h1 style={{ fontSize: '40px', fontWeight: 'bold', marginBottom: '20px' }}>
+								{t('SERVIÇO COMPLETO')}
+							</h1>
+							<p>
+								{t('WP')}
+								{t('WA')}
+							</p>
+						</div>
+					</div>
+					<div className="col-12 mt-5">
+						<div className="container">
+							<div className="row">
+								<div className="col-sm-12 col-md-3 col-lg-3">
+									<div className="ml-4">
+										<img className="ml-5" width="40%" src={support} alt="Card image" />
+									</div>
+									<div className="text-center mr-3">
+										<h3 style={{ marginTop: '42.5px' }}>{t('TS')}</h3>
+										<p> {t('WeP')}</p>
+									</div>
+								</div>
+								<div className="col-sm-12 col-md-3 col-lg-3">
+									<div className="ml-4">
+										<img className="ml-5" width="40%" src={sales} alt="Card image" />
+									</div>
+									<div className="text-center mr-3">
+										<h3 style={{ marginTop: '62.5px' }}>{t('SA')}</h3>
+										<p>{t('AS')}</p>
+									</div>
+								</div>
+								<div className="col-sm-12 col-md-3 col-lg-3">
+									<div className="ml-4">
+										<img className="ml-5" width="40%" src={leadership} alt="Card image" />
+									</div>
+									<div className="mr-3 text-center">
+										<h3 style={{ marginTop: '40px' }}> {t('GUI')}</h3>
+										<p>{t('WePA')}</p>
+									</div>
+								</div>
+								<div className="col-sm-12 col-md-3 col-lg-3">
+									<div className="ml-4">
+										<img className="ml-5" width="40%" src={news} alt="Card image" />
+									</div>
+									<div className="text-center mr-3">
+										<h3 style={{ marginTop: '40px' }}> {t('NE')}</h3>
+										<p>{t('TL')}</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-12 mt-5">
+						<div className="container bg-light">
+							<div className="row mt-3">
+								<div className="col m-3">
+									<h3 className="text-center">{t('OPA')}</h3> <br />
+									<p className="text-center">{t('TC')}</p>
+								</div>
+							</div>
+							<h3 className="text-center h2">{t('DI')}</h3> <br />
+							<div className="row m-1">
+								<div className="col-md-3">
+									<img
+										style={{
+                                            width: '100%',
+                                            height:"195px"
+										}}
+										src={classifieds}
+										alt="classifieds"
+									/>
+									<p style={{ marginTop: '10px' }}>{t('DO')}</p>
+								</div>
+								<div className="col-md-3">
+									<img
+										style={{
+                                            width: '100%',
+                                            height:"195px"
+										}}
+										src={diagnostic}
+										alt="diagnnostic"
+									/>
+									<p style={{ marginTop: '10px' }}>{t('CB')}</p>
+								</div>
+								<div className="col-md-3">
+									<img
+										style={{
+                                            width: '100%',
+                                            height:"195px"
+										}}
+										src={pan}
+										alt="pan"
+									/>
+									<p style={{ marginTop: '10px' }}>
+										{t('D2')} - {t('QA')}
+									</p>
+								</div>
+								<div className="col-md-3 ">
+									<img
+										style={{
+                                            width: '100%',
+                                            height:"195px"
+										}}
+										src={repair}
+										alt="repair"
+									/>
+									<p style={{ marginTop: '10px' }}>{t('DU')}</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-md-12 bg-dark">
+					<div className="container text-white">
+						<div className="row">
+							<div className="col mt-5 mb-5 text-center">
+								<div className="mt-5">
+									<h1 className="text-white">{t('WL')}</h1>
+									<a href="/contact">
+										<button
+											style={{
+												padding: '10px 40px',
+												backgroundColor: 'transparent',
+												border: '1px solid white',
+												color: 'white',
+												borderRadius: '3px',
+												marginTop: '10px'
+											}}
+										>
+											{t('CTL')}
+										</button>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
 export default Service;
