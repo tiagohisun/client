8982736f51ef74209313990c02f4da15c26d764b
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2021-02-12 19:47:25 +0500

    webpack error solved

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-    i18n: {
-        locales: ["en", "es", "fr"],
-        defaultLocale: "en",
-    },
-};
+// module.exports = {
+//     i18n: {
+//         locales: ["en", "es", "fr"],
+//         defaultLocale: "en",
+//     },
+// };
