c2e665b947d5ec96f7cd9c8552fe82d0b925db99
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2021-02-13 11:56:58 +0500

    footer alignment and spacing set

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,23 +3,22 @@ import React, { useState } from 'react';
 import { Form, Input, Button, notification } from 'antd';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import './styles.css'
+import './styles.css';
 //import { AiFillPhone } from "react-icons/ai";
 import { MdEmail, MdLocationOn } from 'react-icons/md';
 
 import logo from '../../static/images/logo/logonb.png';
 import '../../static/css/footer.css';
 
-import {useTranslation} from 'react-i18next'
+import { useTranslation } from 'react-i18next';
 //import emailjs from "emailjs-com";
 const Footer = () => {
-		
 	const [ isModalVisible, setIsModalVisible ] = useState(false);
 	const [ name, setName ] = useState('');
 	const [ email, setEmail ] = useState('');
 	const [ phonenumber, setPhonenumber ] = useState(null);
 	const [ message, setMessage ] = useState('Newsletter');
-	
+
 	const showModal = () => {
 		setIsModalVisible(true);
 	};
@@ -70,7 +69,7 @@ const Footer = () => {
 			span: 12
 		}
 	};
-	const {t, i18n} = useTranslation();
+	const { t, i18n } = useTranslation();
 	return (
 		<div
 			className="footer-container"
@@ -84,7 +83,7 @@ const Footer = () => {
 				<div className="row mt-5">
 					<div className="col-sm-12 col-md-3">
 						<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-							<div style={{ margin: '20px' }}>
+							<div style={{ margin: '5px' }}>
 								<Link to="/">
 									<img style={{ width: '150px', paddingBottom: '20px' }} src={logo} />
 								</Link>
@@ -100,22 +99,20 @@ const Footer = () => {
 								<div style={{ display: 'flex' }}>
 									<div />
 									<div style={{ marginLeft: '10px', fontSize: '15px' }}>
-										Av Yojiro Takaoka, 438, {t("FL")} - Alphaville - SP
-										<br /> 06541-038, {t("BZ")}
+										Av Yojiro Takaoka, 438, {t('FL')} - Alphaville - SP
+										<br /> 06541-038, {t('BZ')}
 									</div>
 								</div>
 								<div>
-									<p style={{ fontSize: '12px', paddingTop: '40px' }}>
-										&copy; {t("CPR")}
-									</p>
+									<p style={{ fontSize: '12px', paddingTop: '40px' }}>&copy; {t('CPR')}</p>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div className="col-sm-12 col-md-6">
 						<div className="row mt-5">
-							<div className="col-4">
-								<p style={{ fontSize: '20px', fontWeight: 'bold' }}>{t("FT")}</p>
+							<div className="col-sm-12 col-md-4 col-lg-4">
+								<p style={{ fontSize: '20px', fontWeight: 'bold' }}>{t('FT')}</p>
 								<div style={{ fontSize: '14px', color: 'grey' }}>
 									<p>
 										<a style={{ color: 'grey' }} href="/posts">
@@ -124,46 +121,46 @@ const Footer = () => {
 									</p>
 									<p>
 										<a style={{ color: 'grey' }} href="/shop">
-											{t("CS")}
+											{t('CS')}
 										</a>
 									</p>
 								</div>
 							</div>
-							<div className="col-4">
-								<p style={{ fontSize: '20px', fontWeight: 'bold' }}>{t("CMP")}</p>
+							<div className="col-sm-12 col-md-4 col-lg-4">
+								<p style={{ fontSize: '20px', fontWeight: 'bold' }}>{t('CMP')}</p>
 								<div style={{ fontSize: '14px', color: 'grey' }}>
 									<p>
 										<a style={{ color: 'grey' }} href="/about">
-											{t("AB")}
+											{t('AB')}
 										</a>
 									</p>
 									<p>
 										<a style={{ color: 'grey' }} href="/about#ourMission">
-											{t("GL")}
+											{t('GL')}
 										</a>
 									</p>
 									<p>
 										<a style={{ color: 'grey' }} href="/about#privacy">
-											{t("PL")}
+											{t('PL')}
 										</a>
 									</p>
 								</div>
 							</div>
-							<div className="col-4">
-								<p style={{ fontSize: '20px', fontWeight: 'bold' }}>{t("GST")}</p>
+							<div className="col-sm-12 col-md-4 col-lg-4">
+								<p style={{ fontSize: '20px', fontWeight: 'bold' }}>{t('GST')}</p>
 								<div style={{ fontSize: '14px', color: 'grey' }}>
 									<p>
 										<a style={{ color: 'grey' }} href="/contact">
-											{t("CT")}
+											{t('CT')}
 										</a>
 									</p>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div className="col-sm-12 col-md-3 mt-5 ">
+					<div className="col-sm-12 col-md-3 col-lg-3 ">
 						<div style={{ marginTop: '30px' }} className="footer-Newsletter">
-							<Form style={{ width: '200px' }}>
+							<Form>
 								<input
 									style={{
 										height: '25px',
@@ -175,7 +172,7 @@ const Footer = () => {
 										backgroundColor: 'lightGray'
 									}}
 									type="text"
-									placeholder={t("YN")}
+									placeholder={t('YN')}
 									required
 									value={name}
 									onChange={(e) => setName(e.target.value)}
@@ -192,7 +189,7 @@ const Footer = () => {
 										backgroundColor: 'lightGray'
 									}}
 									type="email"
-									placeholder={t("YE")}
+									placeholder={t('YE')}
 									required
 									value={email}
 									onChange={(e) => setEmail(e.target.value)}
@@ -209,7 +206,7 @@ const Footer = () => {
 										backgroundColor: 'lightGray'
 									}}
 									type="text"
-									placeholder={t("YN")}
+									placeholder={t('YN')}
 									value={phonenumber}
 									onChange={(e) => setPhonenumber(e.target.value)}
 								/>
@@ -218,22 +215,22 @@ const Footer = () => {
 									<Input.TextArea
 										type="hidden"
 										rows={4}
-										placeholder={t("YP")}
+										placeholder={t('YP')}
 										required
 										value={message}
 										onChange={(e) => setMessage(e.target.value)}
 									/>
 								</Form.Item>
-								<Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 10 }}>
-									<div style={{ textAlign: 'center' }}>
+								<Form.Item>
+									<div>
 										<Button
-											style={{ right: '83px', width: '250px', textAlign: 'center' }}
+											style={{ width: '250px', textAlign: 'center' }}
 											type="primary"
 											htmlType="submit"
 											onClick={() => mailsender()}
 											block
 										>
-											{t("SB")}
+											{t('SB')}
 										</Button>
 									</div>
 								</Form.Item>
