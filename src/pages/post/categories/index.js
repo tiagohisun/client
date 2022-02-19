a4c0eeec53523b00ccb94fe5bacccdf93ab118c5
irabbi360 <fazle08rabbi@gmail.com>
2022-02-19 20:37:33 +0600

    dental category design updated

--- src/pages/post/categories/index.js
+++ src/pages/post/categories/index.js
@@ -5,6 +5,50 @@ import { Pagination } from "antd";
 
 import { getPostsByCount } from "../../../functions/post";
 
+const MainPostCard = ({ post }) => {
+  return (
+    <div
+      className="col-md-4 wow slideInUp mb-4 shadow-lg"
+      data-wow-delay="0.1s"
+      style={{
+        visibility: "visible",
+        animationDelay: "0.1s",
+        animationName: "slideInUp",
+      }}
+    >
+      <div className="blog-item bg-light rounded overflow-hidden">
+        <div
+          className="blog-img position-relative img-thumbnail overflow-hidden"
+          style={{ height: "150px" }}
+        >
+          <img
+            classname="img-fluid"
+            src={
+              post.images.length > 0
+                ? post.images[0].url
+                : "https://www.themepush.com/demo-moschino/wp-content/uploads/sites/15/2015/09/p1.jpg"
+            }
+            alt=""
+            style={{ height: "100%", width: "100%" }}
+          />
+          <Link
+            to={`/posts/category/${post.postcategory.slug}`}
+            key={post.postcategory.slug}
+            className="position-absolute top-0 start-0 bg-primary text-white rounded-end mt-5 py-2 px-4"
+          >
+            {post.postcategory.name}
+          </Link>
+        </div>
+        <div className="px-2">
+          <Link to={`/posts/${post.slug}`} key={post.slug}>
+            <h5 className="my-3">{post.title}</h5>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const PostCard = ({ post }) => {
   return (
     <div
@@ -48,6 +92,82 @@ const PostCard = ({ post }) => {
   );
 };
 
+const DentalPostCardFirst = ({ post, index }) => {
+  return (
+    <div
+      className="col-md-4 wow slideInUp mb-4"
+    >
+      <div className="blog-item rounded overflow-hidden">
+        <div
+          className="blog-img position-relative img-thumbnail border-0 overflow-hidden"
+          style={{ height: "180px" }}
+        >
+          <img
+            classname="img-fluid img-thumbnail border-0"
+            src={
+              post.images.length > 0
+                ? post.images[0].url
+                : "https://www.themepush.com/demo-moschino/wp-content/uploads/sites/15/2015/09/p1.jpg"
+            }
+            alt=""
+          />
+          <Link
+            to={`/posts/category/${post.postcategory.slug}`}
+            key={post.postcategory.slug}
+            className="badge badge-dark text-white py-1"
+            style={{"position":"absolute","top":"160px"}}
+          >
+            {post.postcategory.name}
+          </Link>
+        </div>
+        <div className="px-2">
+          <Link to={`/posts/${post.slug}`} key={post.slug}>
+            <p className="my-3">{post.title}</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const DentalPostCardSecond = ({ post }) => {
+  return (
+    <div
+      className="col-md-6 wow slideInUp mb-4"
+    >
+      <div className="blog-item rounded overflow-hidden">
+        <div
+          className="blog-img position-relative img-thumbnail border-0 overflow-hidden"
+          style={{ height: "180px" }}
+        >
+          <img
+            classname="img-fluid img-thumbnail border-0"
+            src={
+              post.images.length > 0
+                ? post.images[0].url
+                : "https://www.themepush.com/demo-moschino/wp-content/uploads/sites/15/2015/09/p1.jpg"
+            }
+            alt=""
+          />
+          <Link
+            to={`/posts/category/${post.postcategory.slug}`}
+            key={post.postcategory.slug}
+            className="badge badge-dark text-white py-1"
+            style={{"position":"absolute","top":"160px","left": "0"}}
+          >
+            {post.postcategory.name}
+          </Link>
+        </div>
+        <div className="px-2">
+          <Link to={`/posts/${post.slug}`} key={post.slug}>
+            <p className="my-3">{post.title}</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const PostBanner = ({ post }) => {
   return (
     <div
@@ -81,7 +201,7 @@ function CategoryPost() {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [currentLimit, setCurrentLimit] = useState(15);
+  const [currentLimit, setCurrentLimit] = useState(10);
   const [totalResult, setTotalResult] = useState(0);
 
   const [recentposts, setRecentPosts] = useState([]);
@@ -168,10 +288,40 @@ function CategoryPost() {
           <div className="section-title section-title-sm position-relative mb-4" style={{ borderBottom: '3px solid #d8dcdf', fontSize:'20px'}}>
             <h3 className="mb-0 badge badge-dark text-uppercase">{posts.length > 0 ? posts[0].postcategory.name : ''}</h3>
           </div>
+
+          {posts.length > 0 && posts[0].postcategory.slug == 'dental-news' ?
+            <div className="row mb-5">
+                {posts.map((post, index) => {
+                  if(index <= 2) {
+                    return <MainPostCard post={post} />;
+                 }
+                })}
+                <hr style={{"position":"relative","color":"#607d8b","width":"90%","margin":"auto","height":"4px"}}/>
+            </div> : ''
+          }
+
+          {posts.length > 0 && posts[0].postcategory.slug == 'dental-news' ?
+            <div className="row mb-5">
+                {posts.map((post, index) => {
+                  if(index == 3 || index == 4 || index == 5) {
+                    return <DentalPostCardFirst post={post} />;
+                 }
+                })}
+                <hr style={{"position":"relative","color":"#607d8b","width":"90%","margin":"auto","height":"4px"}}/>
+            </div> : ''
+          }
           <div className="row">
-            {posts.map((post) => {
-              return <PostCard post={post} />;
-            })}
+            {posts.length > 0 && posts[0].postcategory.slug == 'dental-news' ? 
+             posts.map((post, index) => {
+               if(index > 5) {
+                return <DentalPostCardSecond post={post} index={index} />;
+               }
+                
+               }) : posts.map((post) => {
+                return <PostCard post={post} />;
+              })
+            }
+            <hr />
           </div>
           <div className="row">
             <Pagination
